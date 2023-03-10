cdc4f0703aec412ce0f6498ec2a062619417d937
André Eriksson <andretech1@hotmail.com>
2023-03-10 07:37:34 +0100

    First Files
    Adding the first files to the repo

---
+++ src/test/java/com/beastlymc/fileuploader/FileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beastlymc.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
