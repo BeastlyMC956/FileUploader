6e4956fc0637109cde73a120f005915c70f1c2df
André Eriksson <andretech1@hotmail.com>
2023-03-17 13:50:33 +0100

    Moved files for consistency

--- src/test/react/App.test.js
+++ src/test/react/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../../main/react/src/App';
 
 test('renders learn react link', () => {
   render(<App />);
